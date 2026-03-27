3ec141cc80b8bf9a4f8a0404ed80d98201a72035
Morkos <morkos.cairo@gmail.com>
2026-03-26 20:16:12 -0700

    Swap link service with custom domain fight.offtherecord.com

--- src/0_config.js
+++ src/0_config.js
@@ -4,6 +4,6 @@
 goog.provide('config');
 
 config.app_service_endpoint = 'https://relay.offtherecord.com:444/app-link';
-config.link_service_endpoint = 'https://bnc.lt';
+config.link_service_endpoint = 'https://fight.offtherecord.com';
 config.api_endpoint = 'https://relay.offtherecord.com:444';
 config.version = '2.74.0';
